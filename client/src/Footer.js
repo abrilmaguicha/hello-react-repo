86115f2eab3080e6a7b427e21e2aaf8014750f2d
abrilmaguicha <67296661+abrilmaguicha@users.noreply.github.com>
2020-10-28 20:31:58 -0300

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="text-center mb-3">
       <hr />
       <h4 className="text-muted">Hello React</h4>
-      <a href="https://github.com/abrilmaguicha/hello-react-repo">
+      <a href="https://github.com/santiagotrini/hello-react">
         <i className="fa fa-github fa-3x text-dark"></i>
       </a>
     </div>
